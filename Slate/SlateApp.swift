226e032ab9d8abad09de37948cd55059948008ea
James McHargue <jpm221@case.edu>
2021-12-22 20:57:56 -0600

    Initial Commit

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by James McHargue on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
